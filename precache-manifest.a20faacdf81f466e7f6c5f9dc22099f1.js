40adbe4c91dc5b4d10c0f7cfdea974aba045202f
MrArcticFox <tromos4ever@gmail.com>
2020-07-11 19:09:07 +0200

    Updates

---
+++ precache-manifest.a20faacdf81f466e7f6c5f9dc22099f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b123ca1d1bdceb6215055a001a44464",
+    "url": "/Catfriends/index.html"
+  },
+  {
+    "revision": "14222c6c28627b131948",
+    "url": "/Catfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4ba30d4dd63e2845c3a2",
+    "url": "/Catfriends/static/css/main.06d20aaa.chunk.css"
+  },
+  {
+    "revision": "14222c6c28627b131948",
+    "url": "/Catfriends/static/js/2.3b5d270e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Catfriends/static/js/2.3b5d270e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba30d4dd63e2845c3a2",
+    "url": "/Catfriends/static/js/main.48596a25.chunk.js"
+  },
+  {
+    "revision": "f1f9fbf94ba2fbfe1690",
+    "url": "/Catfriends/static/js/runtime-main.eef03358.js"
+  },
+  {
+    "revision": "f6f73f2ae978ea568fe2aa47f04be25c",
+    "url": "/Catfriends/static/media/PorterSansBlock-Regular.f6f73f2a.woff"
+  }
+]);
